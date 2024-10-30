9e9d054af3667e01092245bf626833464857da81
HarukaSakate <148409228+HarukaSakate@users.noreply.github.com>
2024-10-30 11:52:39 +0900

    Add files via upload

---
+++ refresh_browser.py
@@ -0,0 +1,19 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+
+url = "https://example.com"  # ここに更新したいページのURLを指定
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+driver.get(url)
+
+try:
+    # 5分ごとにページをリロードするループ
+    while True:
+        time.sleep(300)  
+        driver.refresh()  
+        print("Page reloaded")
+except KeyboardInterrupt:
+    print("Exit script")
+finally:
+    driver.quit()
